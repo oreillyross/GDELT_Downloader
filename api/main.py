4dcb5feba76fb87dcee55a02b1827844529d5566
Faktor 10 <oreillyross@gmail.com>
2024-11-28 11:46:30 +0000

    Add a query for api endpoint, and option to persist connection

--- api/main.py
+++ api/main.py
@@ -1,21 +1,30 @@
 import os
 
 import psycopg2
-from flask import Flask, jsonify
-
-conn = psycopg2.connect(os.environ['DATABASE_URL'])
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
+def get_db_conn():
+  return psycopg2.connect(os.environ['DATABASE_URL'])
+
+
 @app.route('/api/data', methods=['GET'])
 def get_data():
-  cur = conn.cursor()
-  cur.execute('SELECT * FROM your_table')
-  data = cur.fetchall()
-  cur.close()
-  conn.close()
-  return jsonify(data)
-  
-  
+  param_value = request.args.get("country")
+  conn = get_db_conn()
+  try:
+    cur = conn.cursor()
+    query = "SELECT * FROM cameo_countries WHERE label like %s"
+    country_query = f"%{param_value}%"
+    cur.execute(query, (country_query, ))
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
+  finally:
+    conn.close()
 
 
+if __name__ == '__main__':
+  app.run(debug=True)
